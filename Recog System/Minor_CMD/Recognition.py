fca4124232b522cb94e84f4b6a1028755682f88a
ShreyUpadhyaya78 <58781064+ShreyUpadhyaya78@users.noreply.github.com>
2023-03-04 10:00:07 +0545

    Add files via upload

---
+++ Recog System/Minor_CMD/Recognition.py
@@ -0,0 +1,49 @@
+import numpy as np
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import cv2
+import warnings
+warnings.filterwarnings("ignore")
+class Recog:
+    def Recognition(self, img, json_file,weights,characters):
+        def prediction(img,json,weights,characters):
+            json_file = open(json, 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            loaded_model = model_from_json(loaded_model_json)
+            loaded_model.load_weights(weights)
+            loaded_model.save('temporary.hdf5')
+            loaded_model = load_model('temporary.hdf5')
+            roi = cv2.resize(img, (29, 29), interpolation=cv2.INTER_AREA)
+            imgxwithpadding  = np.zeros((32,32),np.uint8)
+            imgxwithpadding[1:30,1:30] = roi
+            roi = imgxwithpadding
+            roi = img
+            ##plt.imshow(roi)
+            ##plt.show()
+            roi = np.array(roi)
+            roi.reshape(1, 1024)
+            prediction = loaded_model.predict(roi.reshape(1, 1024))
+            characters = characters.split(',')
+            output = characters[np.argmax(prediction.reshape(len(characters)))]
+            return output,prediction.reshape(len(characters))
+
+        def classifier(segment,json,weights,characters):
+            pred_lbl = ""
+            acc = []
+            if segment == [] :
+                return ["",0]
+            if segment is not None :
+                segment = cv2.resize(segment, (32, 32))
+                segment = cv2.bitwise_not(segment)
+                lbl = prediction(segment,json,weights,characters)
+                pred_lbl += lbl[0]
+            else :
+                pred_lbl+= ""
+                lbl = ["", 0]
+            return pred_lbl,lbl[1]
+
+        return classifier(img,json_file,weights,characters)
+
+
---
+++ Recog System/Minor_CMD/copy_of_charactersegementation.py
@@ -0,0 +1,119 @@
+import numpy as np
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import cv2
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class Recog:
+    def Recognition(self, img):
+        def prediction(img):
+            json_file = open('middle (1).json', 'r')
+
+            loaded_model_json = json_file.read()
+            json_file.close()
+            loaded_model = model_from_json(loaded_model_json)
+            loaded_model.load_weights("middle (1).h5")
+            loaded_model.save('middle.hdf5')
+            loaded_model = load_model('middle.hdf5')
+            roi = cv2.resize(img, (29, 29), interpolation=cv2.INTER_AREA)
+            imgxwithpadding  = np.zeros((32,32),np.uint8)
+            imgxwithpadding[1:30,1:30] = roi
+            roi = imgxwithpadding
+            ##plt.imshow(roi)
+            ##plt.show()
+            roi = np.array(roi)
+            roi.reshape(1, 1024)
+            prediction = loaded_model.predict(roi.reshape(1, 1024))
+            characters = 'ा,क,ख,ग,घ,ङ,च,छ,ज,झ,ञ,ट,ठ,ड,ढ,ण,त,थ,द,ध,न,प,फ,ब,भ,म,य,र,ल,व,श,ष,स,ह,क्ष,त्र,ज्ञ,०,१,२,३,४,५,६,७,८,९'
+            characters = characters.split(',')
+
+            output = characters[np.argmax(prediction.reshape(47))]
+            return output
+
+        def classifier(segment):
+            pred_lbl = ""
+            acc = []
+
+            segment = cv2.resize(segment, (32, 32))
+            segment = cv2.erode(segment, (3, 3), 1)
+            lbl = prediction(segment)
+            pred_lbl += lbl
+            ##plt.imshow(segment)
+            ##plt.show()
+            return pred_lbl
+
+        return classifier(img)
+
+    def Segmentation(self, ximg,counter):
+        keys2,keys,  ximg = self.keyDetection(ximg)
+        copyForSegmentationShow = ximg.copy()
+        char = ""
+        segmentedimages = []
+        for key in range(0, len(keys) - 1):
+            imgx = ximg[0:ximg.shape[0], keys[key]-2:(keys2[key+1] + keys[key+1])//2+4]
+            cv2.rectangle(copyForSegmentationShow, (keys[key]-2,0), ((keys2[key+1] + keys[key+1])//2+4,ximg.shape[0]), (255, 0,0), 1)
+            sum = 0
+            for i in imgx:
+                for j in imgx:
+                    sum += j
+            if(len(sum) < 20):
+                continue
+            recognizedcharacter  = self.Recognition(imgx)
+            if recognizedcharacter == '८':
+                pass
+            else:
+                char += recognizedcharacter
+
+            segmentedimages.append(imgx)
+        cv2.imwrite("character segmentation/first"+ str(counter)+".png",copyForSegmentationShow)
+        return char
+
+    def keyDetection(self, ximg):
+
+        ximg = cv2.bitwise_not(ximg)
+        ximg = cv2.dilate(ximg, np.ones((3, 3), np.uint8))
+        org  = ximg.copy()
+        kernel = np.ones((5, 5), np.float32) / 30
+        ximg = cv2.filter2D(ximg, -1, kernel)
+        dict = {}
+        for i in range(0, len(ximg[0])):
+            sum = 0
+            for j in range(0, len(ximg)):
+                if ximg[j][i] > 100:
+                    sum += 1
+            dict[i] = sum
+        x = dict.values()
+        x = list(set(x))
+        greater_than_threshold = {k: v for k, v in dict.items() if v >= max(x[0:5])*1.25}
+        current_key = min(greater_than_threshold.keys())
+        current_part = {current_key: greater_than_threshold[current_key]}
+        parts = [current_part]
+        keys = []
+        for key in sorted(greater_than_threshold.keys()):
+            if key == current_key + 1:
+                current_key = key
+                current_part[key] = greater_than_threshold[key]
+            else:
+                current_key = key
+                keys.append(key)
+                current_part = {current_key: greater_than_threshold[key]}
+                parts.append(current_part)
+        keys.append(ximg.shape[1])
+        keys2 = []
+        for key in sorted(greater_than_threshold.keys(),reverse=True):
+            if key == current_key - 1:
+                current_key = key
+                current_part[key] = greater_than_threshold[key]
+            else:
+                current_key = key
+                keys2.append(key)
+                current_part = {current_key: greater_than_threshold[key]}
+                parts.append(current_part)
+        keys2.append(0)
+        ximg = org
+        return  keys2[::-1],keys, ximg
